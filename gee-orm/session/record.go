6241192c83e7df870177bc419ae427956e697608
delete-cloud <1526706882@qq.com>
2024-04-22 22:58:16 +0800

    GeeORM 3rd day, finish insert and select

---
+++ gee-orm/session/record.go
@@ -0,0 +1,50 @@
+package session
+
+import (
+	"geeorm/clause"
+	"reflect"
+)
+
+func (s *Session) Insert(values ...interface{}) (int64, error) {
+	recordValues := make([]interface{}, 0)
+	for _, value := range values {
+		table := s.Model(value).RefTable()
+		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
+		recordValues = append(recordValues, table.RecordValues(value))
+	}
+
+	s.clause.Set(clause.VALUES, recordValues...)
+	sql, vars := s.clause.Build(clause.INSERT, clause.VALUES)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+
+	return result.RowsAffected()
+}
+
+func (s *Session) Find(values interface{}) error {
+	destSlice := reflect.Indirect(reflect.ValueOf(values))
+	destType := destSlice.Type().Elem()                                   // 获取切片的单个元素类型
+	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable() // 使用reflect.New()方法创建一个destType的实例，作为Model()的入参，映射出表结构RefTable()
+
+	s.clause.Set(clause.SELECT, table.Name, table.FieldNames) // 根据表结构，使用clause构造出SELECT语句，查询到所有符合条件的记录rows
+	sql, vars := s.clause.Build(clause.SELECT, clause.WHERE, clause.ORDERBY, clause.LIMIT)
+	rows, err := s.Raw(sql, vars...).QueryRows()
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		dest := reflect.New(destType).Elem()
+		var values []interface{}
+		for _, name := range table.FieldNames {
+			values = append(values, dest.FieldByName(name).Addr().Interface())
+		}
+		if err := rows.Scan(values...); err != nil {
+			return err
+		}
+		destSlice.Set(reflect.Append(destSlice, dest))
+	}
+	return rows.Close()
+}
