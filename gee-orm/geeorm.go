81dcd32225152ade6a35004fbf11f29f8cea78a4
delete-cloud <1526706882@qq.com>
2024-04-25 19:36:16 +0800

    GeeORM 6th day, finish transaction

--- gee-orm/geeorm.go
+++ gee-orm/geeorm.go
@@ -12,6 +12,34 @@ type Engine struct {
 	dialect dialect.Dialect
 }
 
+type TxFunc func(*session.Session) (interface{}, error)
+
+// 用户只需将所有的操作放到一个回调函数中，作为入参传递给 engine.Transaction()
+// 发生任何错误，自动回滚，如果没有错误发生，则提交
+func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := engine.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			_ = s.Rollback() // err is non-nil; don't change it
+		} else {
+			defer func() {
+				if err != nil { // 若Commit失败，则再回滚一次
+					_ = s.Rollback()
+				}
+			}()
+			err = s.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return f(s)
+}
+
 func NewEngine(driver, source string) (e *Engine, err error) {
 	db, err := sql.Open(driver, source)
 	if err != nil {
