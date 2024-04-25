fb34b392b655d85994fe1edc582152994994177b
delete-cloud <1526706882@qq.com>
2024-04-25 20:02:33 +0800

    GeeORM 7th day, finish migrate

--- gee-orm/geeorm.go
+++ gee-orm/geeorm.go
@@ -2,9 +2,11 @@ package geeorm
 
 import (
 	"database/sql"
+	"fmt"
 	"geeorm/dialect"
 	"geeorm/log"
 	"geeorm/session"
+	"strings"
 )
 
 type Engine struct {
@@ -71,4 +73,57 @@ func (engine *Engine) Close() {
 
 func (engine *Engine) NewSession() *session.Session {
 	return session.New(engine.db, engine.dialect) // Replace dialect.MySQL with the appropriate dialect
+}
+
+// difference returns a - b
+func difference(a []string, b []string) (diff []string) {
+	mapB := make(map[string]bool)
+	for _, v := range b {
+		mapB[v] = true
+	}
+	for _, v := range a {
+		if _, ok := mapB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+
+// difference 用来计算前后两个字段切片的差集。新表 - 旧表 = 新增字段， 旧表 - 新表 = 删除字段
+// 使用 ALTER 语句新增字段
+// 使用创建新表并重命名的方式删除字段
+// Migrate table
+func (engine *Engine) Migrate(value interface{}) error {
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := difference(columns, table.FieldNames)
+		log.Infof("added cols %v, deleted cols %v", addCols, delCols)
+
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, f.Name)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
 }
