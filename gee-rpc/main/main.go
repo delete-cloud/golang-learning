d6dedba29065aabb2314236a7487b72b48ba4950
delete-cloud <1526706882@qq.com>
2024-04-30 10:37:19 +0800

    GeeRPC 5th day, finish http protocol support

--- gee-rpc/main/main.go
+++ gee-rpc/main/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 
@@ -18,65 +20,52 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addr chan string) {
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := geerpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
-	}
+	l, _ := net.Listen("tcp", ":9999")
+	_ = geerpc.Register(&foo)
+	geerpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	geerpc.Accept(l)
+	// if err := geerpc.Register(&foo); err != nil {
+	// 	log.Fatal("register error:", err)
+	// }
+
+	// // pick a free port
+	// l, err := net.Listen("tcp", ":0")
+	// if err != nil {
+	// 	log.Fatal("network error:", err)
+	// }
+	// log.Println("start rpc server on", l.Addr())
+	// addr <- l.Addr().String()
+	// geerpc.Accept(l)
 }
-
-func main() {
-	log.SetFlags(0)
-	// 使用了 channel
-	addr := make(chan string) // 无缓冲 channel
-	go startServer(addr)      // 使用协程来运行
-
-	// in fact, following code is like a simple geerpc client
-	client, _ := geerpc.Dial("tcp", <-addr)
+func call(addrCh chan string) {
+	client, _ := geerpc.DialHTTP("tcp", <-addrCh)
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	// send options
-	// _ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
-	// cc := codec.NewGobCodec(conn)
-	var wg sync.WaitGroup
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
 			args := &Args{Num1: i, Num2: i * i}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
-
-			// args := fmt.Sprintf("geerpc req %d", i)
-			// var reply string
-			// if err := client.Call("Foo.Sum", args, &reply); err != nil {
-			// 	log.Fatal("call Foo.Sum error:", err)
-			// }
-			// log.Println("reply:", reply)
 		}(i)
-		// h := &codec.Header{
-		// 	ServiceMethod: "Foo.Sum",
-		// 	Seq:           uint64(i),
-		// }
-		// _ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		// _ = cc.ReadHeader(h)
-		// var reply string
-		// _ = cc.ReadBody(&reply)
-		// log.Println("reply:", reply)
 	}
 	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
--- gee-rpc/server.go
+++ gee-rpc/server.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"reflect"
 	"strings"
 	"sync"
@@ -15,6 +16,12 @@ import (
 )
 
 const MagicNumber = 0x3bef5c
+
+const (
+	connected        = "200 Connected to Gee RPC"
+	defaultRPCPath   = "/_geerpc_"
+	defaultDebugPath = "/debug/geerpc"
+)
 
 type Option struct {
 	MagicNumber    int           // MagicNumber marks this's a geerpc request
@@ -47,6 +54,36 @@ func NewServer() *Server {
 
 // DefaultServer is the default instance of *Server.
 var DefaultServer = NewServer()
+
+// ServeHTTP implements an http.Handler that answers RPC requests.
+func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "CONNECT" {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = io.WriteString(w, "405 must CONNECT\n")
+		return
+	}
+	conn, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		log.Print("rpc hijacking ", req.RemoteAddr, ": ", err.Error())
+		return
+	}
+	_, _ = io.WriteString(conn, "HTTP/1.0 "+connected+"\n\n")
+	server.ServeConn(conn)
+}
+
+// HandleHTTP registers an HTTP handler for RPC messages on rpcPath.
+// It is still necessary to invoke http.Serve(), typically in a go statement.
+func (server *Server) HandleHTTP() {
+	http.Handle(defaultRPCPath, server)
+	http.Handle(defaultDebugPath, debugHTTP{server})
+	log.Println("rpc server debug path:", defaultDebugPath)
+}
+
+// HandleHTTP is a convenient approach for default server to register HTTP handlers
+func HandleHTTP() {
+	DefaultServer.HandleHTTP()
+}
 
 // Accept accepts connections on the listener and serves requests
 // for each incoming connection
@@ -83,13 +120,13 @@ func (server *Server) ServeConn(conn io.ReadWriteCloser) {
 		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
 		return
 	}
-	server.serveCodec(f(conn))
+	server.serveCodec(f(conn), &opt)
 }
 
 // invalidRequest is a placeholder for response argv when error occurs
 var invalidRequest = struct{}{}
 
-func (server *Server) serveCodec(cc codec.Codec) {
+func (server *Server) serveCodec(cc codec.Codec, opt *Option) {
 	sending := new(sync.Mutex) // make sure to send a complete response
 	wg := new(sync.WaitGroup)  // wait until all request are handled
 	for {
@@ -105,7 +142,7 @@ func (server *Server) serveCodec(cc codec.Codec) {
 		}
 		wg.Add(1)
 		// 使用协程并发执行请求
-		go server.handleRequest(cc, req, sending, wg) // 处理请求
+		go server.handleRequest(cc, req, sending, wg, opt.HandleTimeout) // 处理请求
 	}
 	wg.Wait()
 	_ = cc.Close()
